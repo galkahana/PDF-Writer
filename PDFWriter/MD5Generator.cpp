5faf2b3add9fe2c7a151103d0831712e644c94c0
gal kahana <gal.kahana@hotmail.com>
2011-01-29 16:35:54 +0200

    Initial copyright/license work, going for Apache 2.0

--- PDFWriter/MD5Generator.cpp
+++ PDFWriter/MD5Generator.cpp
@@ -1,47 +1,47 @@
-// Gal Kahana 8/5/2010. my code is completely copied/adapted from this:
-//
-// MD5.CC - source code for the C++/object oriented translation and 
-//          modification of MD5.
+/*
+ Additioncal Copyright Information:
 
-// Translation and modification (c) 1995 by Mordechai T. Abzug 
+ Gal Kahana 8/5/2010. my code is completely copied/adapted from this:
 
-// This translation/ modification is provided "as is," without express or 
-// implied warranty of any kind.
+ MD5.CC - source code for the C++/object oriented translation and 
+          modification of MD5.
 
-// The translator/ modifier does not claim (1) that MD5 will do what you think 
-// it does; (2) that this translation/ modification is accurate; or (3) that 
-// this software is "merchantible."  (Language for this disclaimer partially 
-// copied from the disclaimer below).
+ Translation and modification (c) 1995 by Mordechai T. Abzug 
 
-/* based on:
+ This translation/ modification is provided "as is," without express or 
+ implied warranty of any kind.
 
-   MD5C.C - RSA Data Security, Inc., MD5 message-digest algorithm
-   MDDRIVER.C - test driver for MD2, MD4 and MD5
+ The translator/ modifier does not claim (1) that MD5 will do what you think 
+ it does; (2) that this translation/ modification is accurate; or (3) that 
+ this software is "merchantible."  (Language for this disclaimer partially 
+ copied from the disclaimer below).
 
+ the code is based on:
 
-   Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
-rights reserved.
+ MD5.H - header file for MD5C.C
+ MDDRIVER.C - test driver for MD2, MD4 and MD5
 
-License to copy and use this software is granted provided that it
-is identified as the "RSA Data Security, Inc. MD5 Message-Digest
-Algorithm" in all material mentioning or referencing this software
-or this function.
+ Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
+ rights reserved.
 
-License is also granted to make and use derivative works provided
-that such works are identified as "derived from the RSA Data
-Security, Inc. MD5 Message-Digest Algorithm" in all material
-mentioning or referencing the derived work.
+ License to copy and use this software is granted provided that it
+ is identified as the "RSA Data Security, Inc. MD5 Message-Digest
+ Algorithm" in all material mentioning or referencing this software
+ or this function.
 
-RSA Data Security, Inc. makes no representations concerning either
-the merchantability of this software or the suitability of this
-software for any particular purpose. It is provided "as is"
-without express or implied warranty of any kind.
+ License is also granted to make and use derivative works provided
+ that such works are identified as "derived from the RSA Data
+ Security, Inc. MD5 Message-Digest Algorithm" in all material
+ mentioning or referencing the derived work.
 
-These notices must be retained in any copies of any part of this
-documentation and/or software.
+ RSA Data Security, Inc. makes no representations concerning either
+ the merchantability of this software or the suitability of this
+ software for any particular purpose. It is provided "as is"
+ without express or implied warranty of any kind.
 
- */
-
+ These notices must be retained in any copies of any part of this
+ documentation and/or software.
+*/
 #include "MD5Generator.h"
 #include "OutputStringBufferStream.h"
 #include "SafeBufferMacrosDefs.h"
--- PDFWriter/MD5Generator.h
+++ PDFWriter/MD5Generator.h
@@ -1,44 +1,46 @@
-// Gal Kahana 8/5/2010. my code is completely copied/adapted from this:
-//
-// MD5.CC - source code for the C++/object oriented translation and 
-//          modification of MD5.
+/*
+ Additioncal Copyright Information:
 
-// Translation and modification (c) 1995 by Mordechai T. Abzug 
+ Gal Kahana 8/5/2010. my code is completely copied/adapted from this:
 
-// This translation/ modification is provided "as is," without express or 
-// implied warranty of any kind.
+ MD5.CC - source code for the C++/object oriented translation and 
+          modification of MD5.
 
-// The translator/ modifier does not claim (1) that MD5 will do what you think 
-// it does; (2) that this translation/ modification is accurate; or (3) that 
-// this software is "merchantible."  (Language for this disclaimer partially 
-// copied from the disclaimer below).
+ Translation and modification (c) 1995 by Mordechai T. Abzug 
 
-/* based on:
+ This translation/ modification is provided "as is," without express or 
+ implied warranty of any kind.
 
-   MD5.H - header file for MD5C.C
-   MDDRIVER.C - test driver for MD2, MD4 and MD5
+ The translator/ modifier does not claim (1) that MD5 will do what you think 
+ it does; (2) that this translation/ modification is accurate; or (3) that 
+ this software is "merchantible."  (Language for this disclaimer partially 
+ copied from the disclaimer below).
 
-   Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
-rights reserved.
+ the code is based on:
 
-License to copy and use this software is granted provided that it
-is identified as the "RSA Data Security, Inc. MD5 Message-Digest
-Algorithm" in all material mentioning or referencing this software
-or this function.
+ MD5.H - header file for MD5C.C
+ MDDRIVER.C - test driver for MD2, MD4 and MD5
 
-License is also granted to make and use derivative works provided
-that such works are identified as "derived from the RSA Data
-Security, Inc. MD5 Message-Digest Algorithm" in all material
-mentioning or referencing the derived work.
+ Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
+ rights reserved.
 
-RSA Data Security, Inc. makes no representations concerning either
-the merchantability of this software or the suitability of this
-software for any particular purpose. It is provided "as is"
-without express or implied warranty of any kind.
+ License to copy and use this software is granted provided that it
+ is identified as the "RSA Data Security, Inc. MD5 Message-Digest
+ Algorithm" in all material mentioning or referencing this software
+ or this function.
 
-These notices must be retained in any copies of any part of this
-documentation and/or software.
+ License is also granted to make and use derivative works provided
+ that such works are identified as "derived from the RSA Data
+ Security, Inc. MD5 Message-Digest Algorithm" in all material
+ mentioning or referencing the derived work.
 
+ RSA Data Security, Inc. makes no representations concerning either
+ the merchantability of this software or the suitability of this
+ software for any particular purpose. It is provided "as is"
+ without express or implied warranty of any kind.
+
+ These notices must be retained in any copies of any part of this
+ documentation and/or software.
 */
 #pragma once
 
