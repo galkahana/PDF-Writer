21f3d6ddd28ce1e69d026c307a869f8fc8ca3d8d
gal kahana <gal.kahana@hotmail.com>
2012-06-13 01:28:28 +0300

    k. no need for these files

--- PDFWriterTestPlayground/JPGImageBugTest.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  JPGImageBugTest.cpp
-//  PDFWriterTestPlayground
-//
-//  Created by Gal Kahana on 6/12/12.
-//  Copyright (c) 2012 gal.kahana@hotmail.com. All rights reserved.
-//
-
-#include <iostream>
