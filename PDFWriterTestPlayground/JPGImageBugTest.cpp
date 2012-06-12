295bf777f4b7c05efd959b3fd4c4d7709ce15d13
gal kahana <gal.kahana@hotmail.com>
2012-06-13 01:21:31 +0300

    bug correction for JPGs that have bad Photoshop header. turns out this can happen, and i didn't detect for it.

---
+++ PDFWriterTestPlayground/JPGImageBugTest.cpp
@@ -0,0 +1,9 @@
+//
+//  JPGImageBugTest.cpp
+//  PDFWriterTestPlayground
+//
+//  Created by Gal Kahana on 6/12/12.
+//  Copyright (c) 2012 gal.kahana@hotmail.com. All rights reserved.
+//
+
+#include <iostream>
