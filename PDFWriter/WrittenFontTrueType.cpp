65edf8c0c3ec48769d21f58cdfac5642723f0cc0
galkahana <gal.kahana@hotmail.com>
2013-05-13 09:36:26 +0300

    bug, which resolution is to force bullet to require CID encoding for truetype. seems like some mel-treatment in acrobat killing any later text if i don't

--- PDFWriter/WrittenFontTrueType.cpp
+++ PDFWriter/WrittenFontTrueType.cpp
@@ -66,6 +66,17 @@ bool WrittenFontTrueType::AddToANSIRepresentation(	const GlyphUnicodeMappingList
 		{
 			encodingResult.first = false;
 		}
+		else if(0x2022 == it->mUnicodeValues.front())
+		{
+			// From the reference:
+			// In WinAnsiEncoding, all unused codes greater than 40 map to the bullet character. 
+			// However, only code 225 is specifically assigned to the bullet character; other codes are subject to future reassignment.
+
+			// now i don't know if it's related or not...but acrobat isn't happy when i'm using winansi with bullet. and text coming after that bullet may be
+			// corrupted.
+			// so i'm forcing CID if i hit bullet till i know better.
+			encodingResult.first = false;
+		}
 		else
 		{
 			encodingResult = winAnsiEncoding.Encode(it->mUnicodeValues.front());
