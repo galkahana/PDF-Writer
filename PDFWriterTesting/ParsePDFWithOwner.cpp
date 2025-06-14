a9f69990cd7abf77f9834819bda12f66e7a19241
gal kahana <gal.kahana@hotmail.com>
2025-06-14 21:18:09 +0300

    bug: pdfs might have padded us. didnt consider that wonderful option

---
+++ PDFWriterTesting/ParsePDFWithOwner.cpp
@@ -0,0 +1,55 @@
+#include "PDFParser.h"
+#include "EStatusCode.h"
+#include "InputFile.h"
+#include "RefCountPtr.h"
+#include "PDFDictionary.h"
+
+#include <iostream>
+
+#include "testing/TestIO.h"
+
+using namespace PDFHummus;
+
+int ParsePDFWithOwner(int argc, char* argv[])
+{
+    int pIndex = 0;
+	EStatusCode status; 
+    PDFParser parser;
+
+	do
+	{
+        InputFile pdfFile;
+		status = pdfFile.OpenFile(BuildRelativeInputPath(argv, "password.pdf"));
+		if(status != eSuccess)
+		{
+			cout<<"unable to open file for reading. should be in TestMaterials/password.pdf\n";
+			break;
+		}
+
+		status = parser.StartPDFParsing(pdfFile.GetInputStream(), PDFParsingOptions("000000"));
+		if(status != PDFHummus::eSuccess)
+		{
+			cout<<"unable to parse input file";
+			break;
+		}        
+
+        if(!parser.IsEncryptionSupported()) {
+            cout<<"PDFParser does not support encryption, cannot continue\n";
+            status = PDFHummus::eFailure;
+            break;
+        }
+
+        RefCountPtr<PDFDictionary> pageDict(parser.ParsePage(pIndex));
+        if(pageDict.GetPtr() == NULL)
+        {
+            cout<<"unable to parse page "<<pIndex<<"\n";
+            status = PDFHummus::eFailure;
+            break;
+        }
+    }while(false);
+
+
+    return status == eSuccess ? 0:1;
+
+
+}
